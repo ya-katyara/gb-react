780a91fbf92fd3373a1835984a8164123ecb0b15
k@tЯRa <ya_katyara@mail.ru>
2021-03-27 14:27:26 +0300

    Homework 1 webpack config and first react component

---
+++ static_src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MessageField from './message.jsx';
+
+ReactDOM.render(
+   <MessageField />,
+   document.getElementById('root'),
+);
